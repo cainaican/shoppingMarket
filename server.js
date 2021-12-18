21bd7c603205ff0e1e982e3f5a20d2da29bf968d
caincaican <musindinislam1@yandex.ru>
2021-12-18 22:51:21 +0500

    server updated

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     console.log(req.url)
+    const list = ['./index.html', './style.css', './script.js']
+    const body = req.url === '/style.css'
+        ? fs.readFileSync('./style.css')
+        : fs.readFileSync('./index.html')
+    res.end(body)
 })
 
 const port = process.env.PORT || 3000
