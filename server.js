2a6877234a50a04c7392622bb567c36d9cfbac10
caincaican <musindinislam1@yandex.ru>
2021-12-19 12:21:53 +0500

    server with deploying JS

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    const list = ['./index.html', './style.css', './script.js']
-    const body = req.url === '/style.css'
-        ? fs.readFileSync('./style.css')
-        : fs.readFileSync('./index.html')
+    // console.log(req.url)
+    // const body = req.url === '/style.css'
+    //     ? fs.readFileSync('./style.css')
+    //     : fs.readFileSync('./index.html')
+    // 
+    const publicPath = './public'
+    let body = null
+    try {
+        body = fs.readFileSync(`${publicPath}${req.url}`)
+    } catch (e) {
+        console.log(e)
+        body = fs.readFileSync(`${publicPath}/index.html`)
+    }
     res.end(body)
 })
 
